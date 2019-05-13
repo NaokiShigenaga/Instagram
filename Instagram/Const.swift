f552f961eb6281c57309ba1ac3588bc3999e2d80
NaokiShigenaga <n-shigenaga@i-enter.co.jp>
2019-05-13 11:08:08 +0900

    Lesson8-9までのプログラム実装

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 繁永 直希 on 2019/05/10.
+//  Copyright © 2019 naoki.shigenaga. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
